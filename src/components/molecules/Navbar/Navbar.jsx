6c6891277e4bf6dee4c97459a75d1dd461ad2794
syedimran786 <syedimranhgmrd@gmail.com>
2025-03-13 16:32:17 +0530

    toggle icon edited

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import "../Navbar/Navbar.css"
-import logo from "../../../assets/logo.jpg";
+import logo from "../../../assets/delet.gif";
 import ButtonComponent from '../../atoms/ButtonComponent/ButtonComponent';
 import { useAuth } from '../../../App';
 import { Link, animateScroll as scroll } from 'react-scroll';
